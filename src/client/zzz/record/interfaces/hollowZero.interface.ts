f6b3bd1fbd26492711f91778d56a16a7de8ae3b7
Aizuu <64743287+iseizuu@users.noreply.github.com>
2024-08-17 14:33:55 +0700

    feat: added hollow zero

---
+++ src/client/zzz/record/interfaces/hollowZero.interface.ts
@@ -0,0 +1,33 @@
+export interface ZZZHollowZero {
+    abyss_level: {
+        cur_level: number;
+        max_level: number;
+        icon: string;
+    };
+    abyss_point: {
+        cur_point: number;
+        max_point: number;
+    };
+    abyss_duty: {
+        cur_duty: number;
+        max_duty: number;
+    };
+    abyss_talent: {
+        cur_talent: number;
+        max_talent: number;
+    };
+    refresh_time: number;
+    abyss_collect: {
+        type: number;
+        cur_collect: number;
+        max_collect: number;
+    }[];
+    abyss_nest: {
+        is_nest: boolean;
+    };
+    abyss_throne: {
+        is_throne: boolean;
+        max_damage?: string;
+    };
+    unlock: boolean;
+}
--- src/client/zzz/record/record.ts
+++ src/client/zzz/record/record.ts
@@ -3,11 +3,17 @@ import { NodeHoyoError } from '../../../error'
 import { LanguageEnum } from '../../../language'
 import { HTTPRequest } from '../../../request'
 import {
-  ZZZ_RECORD_CHARACTER_API, ZZZ_RECORD_NOTE_API, ZZZ_RECORD_INDEX_API, ZZZ_RECORD_SHIYU_DEFENSE, ZZZ_RECORD_ABYSS
+  ZZZ_RECORD_CHARACTER_API,
+  ZZZ_RECORD_NOTE_API,
+  ZZZ_RECORD_INDEX_API,
+  ZZZ_RECORD_SHIYU_DEFENSE,
+  ZZZ_RECORD_ABYSS,
+  ZZZ_RECORD_HOLLOW_ZERO,
 } from '../../../routes'
 import { ZZZCharacterFull, ZZZNote, ZZZRecord } from './interfaces'
 import { ZZZShiyuDefense } from './interfaces/shiyuDefense.interface';
 import { ZZZAbyss, ZZZAbyssType } from './interfaces/abyss.interface';
+import { ZZZHollowZero } from './interfaces/hollowZero.interface';
 
 /**
  * HSRRecordModule class provides methods to interact with Honkai Star Rail record module endpoints.
@@ -77,6 +83,13 @@ export class ZZZRecordModule {
     return res.data as ZZZRecord
   }
 
+  /**
+   * Retrieves the characters associated with the provided region and UID.
+   *
+   * @returns {Promise<ZZZCharacterFull[]>} A Promise that resolves to an array of ZZZCharacterFull objects.
+   * @throws {NodeHoyoError} if the region or UID parameters are missing or failed to be filled.
+   * @throws {NodeHoyoError} if failed to retrieve data, please double-check the provided UID.
+   */
   async characters(): Promise<ZZZCharacterFull[]> {
     if (!this.region || !this.uid) {
       throw new NodeHoyoError('UID parameter is missing or failed to be filled')
@@ -116,6 +129,13 @@ export class ZZZRecordModule {
     return data.avatar_list as ZZZCharacterFull[]
   }
 
+  /**
+   * Retrieves the note associated with the provided region and UID.
+   *
+   * @returns {Promise<ZZZNote>} A Promise that resolves to the ZZZ note object.
+   * @throws {NodeHoyoError} if the region or UID parameters are missing or failed to be filled.
+   * @throws {NodeHoyoError} if failed to retrieve data, please double-check the provided UID.
+   */
   async note(): Promise<ZZZNote> {
     if (!this.region || !this.uid) {
       throw new NodeHoyoError('UID parameter is missing or failed to be filled')
@@ -155,6 +175,61 @@ export class ZZZRecordModule {
     return res.data as ZZZNote
   }
 
+  /**
+   * Retrieves the hollow zero data associated with the provided region and UID.
+   *
+   * @returns {Promise<ZZZHollowZero>} A Promise that resolves to the ZZZ hollow zero object.
+   * @throws {NodeHoyoError} if the region or UID parameters are missing or failed to be filled.
+   * @throws {NodeHoyoError} if failed to retrieve data, please double-check the provided UID.
+   */
+  async hollowZero(): Promise<ZZZHollowZero> {
+    if (!this.region || !this.uid) {
+      throw new NodeHoyoError('UID parameter is missing or failed to be filled')
+    }
+
+    this.request
+      .setQueryParams({
+        server: this.region,
+        role_id: this.uid,
+        lang: this.lang,
+      })
+      .setDs(true)
+
+    const {
+      response: res,
+      body,
+      params,
+      headers,
+    } = await this.request.send(ZZZ_RECORD_HOLLOW_ZERO)
+
+    if (res.retcode !== 0) {
+      throw new NodeHoyoError(
+        res.message ??
+        'Failed to retrieve data, please double-check the provided UID.',
+        res.retcode,
+        {
+          response: res,
+          request: {
+            body,
+            headers,
+            params,
+          },
+        },
+      )
+    }
+
+    return res.data as ZZZHollowZero
+  }
+
+  /**
+   * Retrieves the Shiyu Defense data for the given schedule type.
+   *
+   * @param {ShiyuDefense} scheduleType - The schedule type of the Shiyu Defense, either CURRENT or PREVIOUS. Defaults to CURRENT.
+   * @returns {Promise<ZZZShiyuDefense>} A Promise that resolves with the Shiyu Defense data.
+   * @throws {NodeHoyoError} if the region or UID parameters are missing or failed to be filled.
+   * @throws {NodeHoyoError} if the given scheduleType parameter is invalid.
+   * @throws {NodeHoyoError} if failed to retrieve data, please double-check the provided UID.
+   */
   async shyiyuDefense(
     scheduleType: ShiyuDefense = ShiyuDefense.CURRENT,
   ): Promise<ZZZShiyuDefense> {
@@ -204,14 +279,14 @@ export class ZZZRecordModule {
     return res.data as ZZZShiyuDefense
   }
 
-/**
- * Retrieves information about the player's performance in the specified abyss type.
- *
- * @param {ZZZAbyssType} abyssType - The type of abyss to retrieve information for. Defaults to 'WitheringGarden'.
- * @return {Promise<ZZZAbyss>} A Promise that resolves with an object containing the player's abyss data.
- * @throws {NodeHoyoError} if UID parameter is missing or failed to be filled, or if the given abyss type parameter is invalid.
- * @throws {NodeHoyoError} if failed to retrieve data, please double-check the provided UID.
- */
+  /**
+   * Retrieves information about the player's performance in the specified abyss type.
+   *
+   * @param {ZZZAbyssType} abyssType - The type of abyss to retrieve information for. Defaults to 'WitheringGarden'.
+   * @return {Promise<ZZZAbyss>} A Promise that resolves with an object containing the player's abyss data.
+   * @throws {NodeHoyoError} if UID parameter is missing or failed to be filled, or if the given abyss type parameter is invalid.
+   * @throws {NodeHoyoError} if failed to retrieve data, please double-check the provided UID.
+   */
   async abyss(abyssType: ZZZAbyssType = 'WitheringGarden'): Promise<ZZZAbyss> {
     if (!this.region || !this.uid) {
       throw new NodeHoyoError('UID parameter is missing or failed to be filled')
@@ -255,6 +330,16 @@ export class ZZZRecordModule {
     }
 
     return res.data as ZZZAbyss
+  }
+
+  /**
+   * Retrieves the URL of a character's avatar based on the provided avatar ID.
+   *
+   * @param {number} avatarId - The ID of the character's avatar.
+   * @return {string} The URL of the character's avatar.
+   */
+  getCharacterAvatar(avatarId: number): string {
+    return `https://act-webstatic.hoyoverse.com/game_record/zzz/role_square_avatar/role_square_avatar_${avatarId}.png`
   }
 }
 
